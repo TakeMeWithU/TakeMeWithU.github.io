d5e595df0ef615759882be5e3338167b63128ba3
Minh <minh.lebao99@gmail.com>
2021-10-26 23:10:42 +0200

    d
    d

---
+++ assets/data/getString.py
@@ -0,0 +1,26 @@
+import csv
+
+class Meal:
+    def __init__(self, id, name, category, price, quantity):
+        self.id = id
+        self.name = name
+        self.category = category
+        self.price = price
+        self.quantity = quantity
+
+listMeal = []
+
+# Using readlines()
+file1 = open('C:\Users\minhl\Documents\MEGAsync\Documents\Atelier de Mi\AtelierDeMi-Site\assets\data\Plats.csv', 'r')
+lines = file1.readlines()
+lines.pop(0)
+for line in lines:
+    line = line.replace('\n', '')
+    values = line.split(';')
+    html = ''
+    html += '<div class="col-lg-6 menu-item filter-' + values[3] + '">'
+    html += 'class="menu-img" alt="' + values[1] + '"/>'
+    html += '<div class="menu-content"><a>'
+    html += values[1] + '</a><span>' + values[4] + '€</span></div>'
+    html += '<div class="menu-ingredients">'+ values[2] + values[5] +'</div></div>'
+    print(html)
